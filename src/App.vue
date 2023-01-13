e1f632bbdbcef9947131458c969dff2b5d6e07ca
platisam <platisam@gmail.com>
2023-01-14 00:04:39 +0100

    novi projekat, header footer

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+</script>
+
+<template>
+  <AppHeader />
+  <AppFooter />
+
+  <RouterView />
+</template>
+
+
---
+++ src/components/icons/IconTooling.vue
@@ -0,0 +1,4 @@
+<!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
+<template>
+ 
+</template>
